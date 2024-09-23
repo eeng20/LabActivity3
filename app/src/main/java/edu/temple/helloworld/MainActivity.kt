84a97a9b5f5a5877592ebba9495d14a768678d74
eeng20 <tul40997@temple.edu>
2024-09-23 15:41:57 -0400

    added Error to empty response

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        nameEditText = findViewById(R.id.nameEditText)
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-
+            if (findViewById<EditText>(R.id.nameEditText).text.toString().isNotEmpty()) {
                 displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
-
+            }
+            else {
+                nameEditText.setError("Must enter a name")
+            }
         }
 
 
